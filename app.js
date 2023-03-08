28e7812d839d445296266d075a7fcb600a93a0d4
shoaib-06 <shoaiblatif090@gmail.com>
2023-03-08 20:30:42 +0500

    minor changes

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("Hello World!");
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
