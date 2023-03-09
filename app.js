6b195902a9cea69a5550c71a850e72273fd1bc54
shoaib-06 <shoaiblatif090@gmail.com>
2023-03-09 18:44:08 +0500

    Completed

--- app.js
+++ app.js
@@ -7,15 +7,76 @@ yearEl.textContent = currentYear;
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
+const allLinks = document.querySelectorAll("a");
 
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute('href');
+    console.log(href);
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+
+        top: 0,
+        behavior: "smooth"
+      }
+      )
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth"
+      })
+    }
+
+    // Close mobile navigation
+    const headerEl = document.querySelector(".header");
+    if (link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle("nav-open")
+    }
+  })
+});
 
 
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
+const heroSec = document.querySelector(".hero-section")
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky")
+    } else if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky")
+    }
+  }, {
+  /* IN THE VIEWPORT */
+  root: null,
+  threshold: 0,
+  rootMargin: "-108px"
+}
+);
+obs.observe(heroSec);
 
 
 ///////////////////////////////////////////////////////////
